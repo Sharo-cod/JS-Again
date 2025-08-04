e64d80285f285bbe5e15e6c77c80339313db993d
Shariq Alam <sharo4192@gmail.com>
2025-08-04 14:30:33 +0500

    datatypes Complete

---
+++ js all/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // Treat all JS Code as newer Version
+
+// alert(3 + 3) // We are Using Nodejs not Browser Thats why it isnt working ..
+
+
+let name = "shariq Alam";
+let age = 18;
+let isLoggedIn = false ;
+
+// number = 2 to power 53 
+// bigInt
+// string => ""
+// boolean => true/false
+// null standalone value
+// undefined 
+// symbol => unique
+
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(console.log(typeof null); // object
+);
+
+
