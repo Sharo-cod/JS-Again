e7dc4ff9a66508e7e1556fb92e9279def99cc2e4
Shariq Alam <sharo4192@gmail.com>
2025-08-04 14:08:28 +0500

    variables complete

---
+++ variables1.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "Sharo4192@gmail.com"
+var accountPass = "23478623894"
+accountCity = "Mingora"
+let accountState;
+// accountId = 2         // not allowed
+
+accountEmail = "asfjb@gmail.com"
+accountPass = "sdfsdjfb skl"
+accountCity = "karachi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState]) 
+
