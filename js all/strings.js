9f1ee6b683344f813c14d0f9afc2b7f3937caf1e
Shariq Alam <sharo4192@gmail.com>
2025-08-05 13:28:35 +0500

    Strings Complete

---
+++ js all/strings.js
@@ -0,0 +1,34 @@
+const name = "Shariq"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello My Name Is ${name} And My RepoCount Is ${repoCount}`);
+
+
+const gameName = new String("sharo_7")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt('4'));
+console.log(gameName.indexOf('s'));
+
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
+const anotherString = gameName.slice(-6,4)
+console.log(anotherString);
+
+
+const newStringOne = "    Shariq    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
