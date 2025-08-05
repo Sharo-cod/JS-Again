8cebeb29bdc819ddc17a8967556202906444ee4b
Shariq Alam <sharo4192@gmail.com>
2025-08-05 09:33:16 +0500

    Comparisons Complete

---
+++ js all/comparisons.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1 );
+// console.log(2 >= 1 );
+// console.log(2 < 1 );                        // Easy == Answer Will Be True Or False
+// console.log(2 == 1 );
+// console.log(2 != 1 );
+ 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------
