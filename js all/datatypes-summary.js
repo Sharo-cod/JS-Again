7b5a78f39b3893dfbd56bd044bfb1101fae6b067
Shariq Alam <sharo4192@gmail.com>
2025-08-05 13:13:29 +0500

    Datatypes complete

---
+++ js all/datatypes-summary.js
@@ -0,0 +1,34 @@
+// # Primitive Datatypes
+
+const score = 100
+const scoreValue = 100.3 
+
+const isLoggedIn = false 
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+// const bigNumber = 38934343434343434343434552348953489n
+
+// # non Primitive Datatypes
+
+const heros = ["superman" , "spiderman", "hulk"]
+let myObj = {
+    name : "Shariq",
+    age : 22,
+}
+
+
+const myFunction = function(){
+    console.log("hello World");
+    
+}
+console.log(typeof outsideTemp);
+
+// -------------------------------------------------------------------------------------------------------------
