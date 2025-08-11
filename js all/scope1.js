4d699ddfb6a10b6426167c04a80d56f49a4dbf4d
Shariq Alam <sharo4192@gmail.com>
2025-08-11 12:50:08 +0500

    part 1 scopes

---
+++ js all/scope1.js
@@ -0,0 +1,18 @@
+// var c = 400 // We dont use var
+
+let a = 300
+
+if (true) {
+    let a = 10
+    const b = 20
+    // var c = 30          // We dont use var
+   console.log("Inner :", a);
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//  =========================================================================================================================================
