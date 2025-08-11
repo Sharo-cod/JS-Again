897871df5fee607d631443c0d2a5ed66d6037230
Shariq Alam <sharo4192@gmail.com>
2025-08-11 13:21:30 +0500

    Scopes Complete

--- js all/scope1.js
+++ js all/scope1.js
@@ -6,13 +6,52 @@ if (true) {
     let a = 10
     const b = 20
     // var c = 30          // We dont use var
-   console.log("Inner :", a);
+//    console.log("Inner :", a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 
 //  =========================================================================================================================================
+
+function one(){
+    const userName = "Shariq"
+    function two(){
+        const website = "Youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+    
+    two()
+}
+// one()
+
+if (true) {
+    const userName = "Sharo"
+    if (userName === "Sharo") {
+        const website = "Google"
+        // console.log(userName + website);
+    }
+    // console.log(website); // Error 1
+}
+
+// console.log(userName); // Error 2
+
+
+// ++++++++++++++++++++++++++  Intresting ++++++++++++++++++++++++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+console.log(addone(5));
+ 
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
