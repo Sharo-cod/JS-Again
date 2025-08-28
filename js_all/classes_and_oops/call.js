40a6616b283efbb27f5a7d378153b9215e7493c1
Shariq Alam <sharo4192@gmail.com>
2025-08-28 10:45:52 +0500

    Call In OOPS

---
+++ js_all/classes_and_oops/call.js
@@ -0,0 +1,14 @@
+function SetUsername(username){
+    // Complex DB calls
+    this.username = username
+    console.log("Called");
+}
+
+function createUser(username,email,password){
+    SetUsername.call(this, username)
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("Chai" , "chai@gmail.com" , "123")
+console.log(chai);
